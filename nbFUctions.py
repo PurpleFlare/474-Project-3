3917ad43b196b71523479be6316cf5dba51deed1
Pranav Kadam <pdkadam@buffalo.edu>
2019-05-10 12:24:41 -0400

    Final changes
    Added the case for when the value isn't found in the dictionary and fixed some bugs. WE"RE DONE!

--- nbFUctions.py
+++ nbFUctions.py
@@ -1,7 +1,7 @@
 from sklearn.base import BaseEstimator
 import numpy as np
 import scipy.stats as stats
-
+from collections import Counter
 # For this assignment we will implement the Naive Bayes classifier as a
 # a class, sklearn style. You only need to modify the fit and predict functions.
 # Additionally, implement the Disparate Impact measure as the evaluateBias function.
@@ -12,19 +12,19 @@ class NBC(BaseEstimator):
     '''
 
     def __init__(self, a=1, b=1, alpha=1):
-        self.__a = a
-        self.__b = b
-        self.__alpha = alpha
+        self.a = a
+        self.b = b
+        self.alpha = alpha
         self.__params = None
         
     def get_a(self):
-        return self.__a
+        return self.a
 
     def get_b(self):
-        return self.__b
+        return self.b
 
     def get_alpha(self):
-        return self.__alpha
+        return self.alpha
 
     # you need to implement this function
 
@@ -43,49 +43,49 @@ class NBC(BaseEstimator):
         params = {}
         # remove next line and implement from here
         # you are free to use any data structure for paramse
-        #i = 0
-        #yes = 0
-        #no = 0
-        #for i in range y.shape[0]:
-        #    if y.at(i) == 1 :
-        #        yes = yes +1
-        #    else:
-        #        no = no + 1
-        #bayes1 = (yes + a) / (y.size + a + b)
-        #bayes2 = 1 - bayes1
-        #params["y=1"] = bayes1
-        #params["y=2"] = bayes2
         
-        for i in range y.size-1:
-        oneValues = np.where(y[i]==1)
-        twoValues = np.where(y[i]==2)
-        
+        oneValues = np.count_nonzero(y == 1)
+        twoValues = np.count_nonzero(y == 2)
+    
         theta1 = (oneValues + a) / (y.size + a + b)
         theta2 = 1 - theta1
         
-        params["y=1"] = theta1
-        params["y=2"] = theta2
-
+        thetaTuple = (theta1, theta2)
+        list_Y1 = {}
+        list_Y2 = {}
+      
+        for j in range(X.shape[1]):
+            list_Y1[j] = {}
+            list_Y2[j] = {}
+           
+            kJ = np.unique(X[:,j]).shape[0]
+            mJ = np.max(X[:,j])
+            
+            for k in range (0, mJ+1):
+                s = 0
+                s2 = 0
+                
+                for l in range (X.shape[0]):
+                    if k == X[l][j] and y[l] == 1:
+                        s = s + 1
+                for l in range (X.shape[0]):
+                    if k == X[l][j] and y[l] == 2:
+                        s2 = s2 + 1 
+                
+                ans1 = (s + alpha) / (oneValues + kJ*alpha)
+                
+                ans2 = (s2 + alpha) / (twoValues + kJ*alpha)
+                
+                list_Y1[j][k] = ans1
+                list_Y2[j][k] = ans2
         
         
-        for j in range X.shape[1]:
-            #the Kj value
-            temp = np.unique(X.at(j))
-            for k in range temp.shape[0]
-                temp1 = np.where(X[j] == temp[k])
-                temp2 = np.where(y == 1) 
-                temp4 = np.where(y == 2)
-                temp3 = np.intersect(temp1, temp2)
-                temp5 = np.intersect(temp1, temp4)
-                
-                prob1 = temp3.size / temp1.size
-                prob2 = temp5.size / temp1.size
-                
-                total1 = (prob1 + alpha)  / (oneValues + temp.size*alpha)
-                total2 = (prob2 + alpha) / (twoValues + temp.size*alpha)
-                
-                params["X =" + j + " | y = 1"] = total1    
-                params["X =" + j + " | y = 2"] = total2
+            list_Y1[j][-1] = (alpha) / (oneValues + kJ*alpha)
+            list_Y2[j][-1] = (alpha) / (twoValues + kJ*alpha)
+            
+        params = (thetaTuple, list_Y1, list_Y2)
+        
+       
         # do not change the line below
         self.__params = params
     
@@ -105,7 +105,42 @@ class NBC(BaseEstimator):
         b = self.get_b()
         alpha = self.get_alpha()
         #remove next line and implement from here
-        
+        j = 0
+        predictions = []
+        for j in range (Xtest.shape[0]):
+            y1 = params[0][0]
+            y2 = params[0][1]
+            
+            #find given product of Xj when y = 1 and y= 2
+            prod1 = 1
+            prod2 = 1
+            for k in range (Xtest.shape[1]):
+                temp = Xtest[j][k] 
+                
+                #when attribute k has y = 1
+                #when attribute k has y = 1
+                #1 means the first library
+                #k means each attribute
+                #temp is the feature of kth attribute that jth person has
+                if(temp in params[1][k].keys()):
+                    val1 = params[1][k][temp]
+                else:
+                    val1 = params[1][k][-1]
+                if(temp in params[2][k].keys()):
+                #when attribute k has y = 2
+                    val2 = params[2][k][temp]
+                else:
+                    val2 = params[2][k][-1]
+                prod1 = prod1 * val1
+                prod2 = prod2 * val2
+            
+            forYes = (y1 * prod1) / ((y1 * prod1) + (y2 * prod2))
+            forNo = (y2 * prod2) / ((y2 *prod2) + (y1 * prod1))
+            
+            if forYes > forNo:
+                predictions.append(1)
+            else:
+                predictions.append(2)
         #do not change the line below
         return predictions
         
@@ -121,26 +156,26 @@ def evaluateBias(y_pred,y_sensitive):
     Output:
     di (disparateimpact): scalar value
     '''
-    #remove next line and implement from here
-    int i = 0
-    #getting value with bad credit
-    int y2
-    for i in y_pred:
-        if y_pred.at(i) == 2:
-            y2 += 1
-    #get unprivileged value from y_sensitive
-    int j = 0
-    int sucks2Bme
-    int imOKtho
-    for j in y_sensitive:
-        if y_sensitive.at(i) == 2:
-            sucks2Bme += 1
-        else:
-            imOKtho += 1
+    arrayS = np.count_nonzero(y_sensitive == 1)
+    arrayNS = np.count_nonzero(y_sensitive == 2)
     
-    di = (y2 * imOKtho) / (y2 * sucks2Bme)
+  
+    numeratorVal = 0
+    denominatorVal = 0
     
-    #do not change the line below
+    for i in range(y_pred.size):
+        if (y_pred[i] == 2 and y_sensitive[i] != 1):
+            numeratorVal += 1
+   
+        elif (y_pred[i] == 2 and y_sensitive[i] == 1):
+            denominatorVal += 1
+            
+            
+    numeratorVal = numeratorVal/arrayNS
+    denominatorVal = denominatorVal/arrayS
+    
+    di = numeratorVal/denominatorVal
+
     return di
 def genBiasedSample(X,y,s,p,nsamples=1000):
     '''
